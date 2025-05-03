d0d8fe4d8e5cfdd94e2dcd079921f2d44b4538c8
vungnv <vungnv@avtvn.com>
2025-05-03 15:23:44 +0700

    change mint func cho test

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,11 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-	constructor(uint256 initSupply) ERC20("MyToken", "MTK") Ownable(msg.sender) {
-		_mint(msg.sender, initSupply);
-	}
+    constructor(
+        uint256 initSupply
+    ) ERC20("MyToken", "MTK") Ownable(msg.sender) {
+        _mint(msg.sender, initSupply);
+    }
 
-	function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
+        if (balanceOf(to) >= 200 * (10 ** decimals())) {
+            require(
+                msg.sender == owner(),
+                "Only owner can mint to this address"
+            );
+        }
         _mint(to, amount);
     }
 }
--- contracts/NewToken.sol
+++ contracts/NewToken.sol
@@ -5,11 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NewToken is ERC20, Ownable {
-	constructor(uint256 initialSupply) ERC20("NewToken", "NTK") Ownable(msg.sender){
-		_mint(msg.sender, initialSupply);
-	}
+    constructor(
+        uint256 initialSupply
+    ) ERC20("NewToken", "NTK") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
 
-	function mint(address to, uint256 amount) public onlyOwner {
-		_mint(to, amount);
-	}
+    function mint(address to, uint256 amount) public {
+        if (balanceOf(to) >= 200 * (10 ** decimals())) {
+            require(
+                msg.sender == owner(),
+                "Only owner can mint to this address"
+            );
+        }
+        _mint(to, amount);
+    }
 }
